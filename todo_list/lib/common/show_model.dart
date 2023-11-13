7387fea68a95d3b6d0c836b90d1554beae4b7c28
Baizme <baizmeabm@gmail.com>
2023-11-13 09:41:11 +0700

    showmodelradiowidget

--- todo_list/lib/common/show_model.dart
+++ todo_list/lib/common/show_model.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:todo_list/constants/app_style.dart';
+import 'package:todo_list/widget/radio_widget.dart';
+import 'package:todo_list/widget/textfield_widget.dart';
 
 class AddNewTaskModel extends StatelessWidget {
   const AddNewTaskModel({
@@ -34,12 +38,59 @@ class AddNewTaskModel extends StatelessWidget {
             thickness: 1.2,
             color: Colors.grey.shade200,
           ),
-          SizedBox(height: 12),
-          Text(
+          const Gap(12),
+          const Text(
             'Title Task',
             style: AppStyle.headingOne,
           ),
-          TextField(),
+          const Gap(6),
+          const TextFieldWidget(maxLine: 1, hintText: 'Add Task Name'),
+          const Gap(12),
+          const Text('Description', style: AppStyle.headingOne),
+          Gap(6),
+          const TextFieldWidget(maxLine: 5, hintText: 'Add Descriptions'),
+          Gap(12),
+          const Text('Category', style: AppStyle.headingOne),
+          Row(
+            children: [
+              Expanded(
+                child: RadioWidget(categColor: Colors.green, titleRadio: 'LRN'),
+              ),
+              Expanded(
+                child: RadioWidget(
+                    categColor: Colors.blue.shade700, titleRadio: 'WRK'),
+              ),
+              Expanded(
+                child: RadioWidget(
+                    categColor: Colors.amberAccent.shade700, titleRadio: 'GEN'),
+              ),
+            ],
+          ),
+
+          //Date time Section
+
+          Row(
+            children: [
+              Container(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text('Date', style: AppStyle.headingOne),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade200,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Row(children: const [
+                          Icon(CupertinoIcons.calendar),
+                          Gap(12),
+                          Text('dd/mm/yy'),
+                        ]),
+                      ),
+                    ]),
+              )
+            ],
+          )
         ],
       ),
     );
